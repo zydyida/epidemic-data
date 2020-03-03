0f54ded4b9d1077569dc44bd0f1aecd12b6ce278
Yida Zhu <33805303+zydyida@users.noreply.github.com>
2020-03-03 18:16:32 +0800

    Update dataChinaCity.py

--- dataChinaCity.py
+++ dataChinaCity.py
@@ -10,12 +10,9 @@ number = format(time.time() * 100, '.0f')
 url = 'https://view.inews.qq.com/g2/getOnsInfo?name=disease_h5&callback=&_=%s' % number
 datas = json.loads(requests.get(url=url).json()['data'])
 
-print('更新时间：' + datas['lastUpdateTime'])
-
 #写入表头
 with open(ExcelName, 'w', encoding='utf_8_sig', newline='') as csvfile:#utf_8_sig替换utf-8解决中文乱码问题
     writer = csv.writer(csvfile)
-    writer.writerow(["记录时间", str(datas['lastUpdateTime'])])
     writer.writerow(["省份","城市","新增确诊","累计确诊","治愈","死亡"])
     
 for contry in datas['areaTree']:
